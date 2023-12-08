7582b51170d8c5b75c5aa70d632014f54bd2d312
mtk2608 <150594024+mtk2608@users.noreply.github.com>
2023-12-08 03:00:03 +0200

    Commit3

--- Assignment2.Sln/Assignment2/Program.cs
+++ Assignment2.Sln/Assignment2/Program.cs
@@ -1,13 +1,82 @@
-﻿namespace Assignment2
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment2
 {
     internal class Program
     {
+        public static void Main(string[] args) 
+        
+        
+        {
+            Console.WriteLine("Enter Up To 4 Digits");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Wordels.ConvertNumberToWords(input));
+        }
 
+        
+
+
+        /*static string ConvertNumberToWords(int number)
+            {
+                if (number == 0)
+                    return "Zero";
+
+                if (number < 0)
+                    return "Minus " + ConvertNumberToWords(Math.Abs(number));
+
+                string words = "";
+
+                if ((number / 1000) > 0)
+                {
+                    words += ConvertNumberToWords(number / 1000) + " Thousand ";
+                    number %= 1000;
+                }
+
+                if ((number / 100) > 0)
+                {
+                    words += ConvertNumberToWords(number / 100) + " Hundred ";
+                    number %= 100;
+                }
+
+                if (number > 0)
+                {
+                    if (words != "")
+                        words += " ";
+
+                    var unitsMap = new[]
+                    {
+                    "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+                    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+                    };
+
+                    var tensMap = new[]
+                    {
+                    "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+                    };
+
+                    if (number < 20)
+                        words += unitsMap[number];
+                    else
+                    {
+                        words += tensMap[number / 10];
+                        if ((number % 10) > 0)
+                            words += "-" + unitsMap[number % 10];
+                    }
+                }
+
+                return words;
+
+                //Console.WriteLine(words);
+            }
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number up to 4 Digits");
-            Console.ReadLine();
+            Console.WriteLine("Enter Up To 4 Digits");
+            int input = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine(ConvertNumberToWords(input));
+            
+        }*/
 
-        }
     }
+    
 }
---
+++ Assignment2.Sln/Assignment2/Wordels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Wordels
+    {
+        public static string ConvertNumberToWords(int numbers) 
+
+        {
+
+            string[] SingleDigit =new string[] { " ", "One" , "Two" , "Three" , "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+            string[] DoubleDigit = new string[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+            string[] Tens = new string[] {" ", " ", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+            if (numbers == 0)
+            {
+                return "zero";
+            }
+         
+            else if (numbers > 0 & numbers < 10)
+            {
+                return SingleDigit[numbers];
+            }
+                
+
+            else if (numbers >= 10 & numbers < 20)
+            {
+                
+                return DoubleDigit[numbers-10];
+            }
+
+            else if (numbers > 20 & numbers < 100) 
+            {
+                
+                return Tens[numbers/10] + " " + SingleDigit[numbers%10];
+               
+            }
+
+            /*else if (numbers>100) 
+            {
+                return SingleDigit[numbers/100] + " Hundred" + " " + Tens[numbers%100/10] + DoubleDigit[numbers%100%10];
+            }*/
+                
+
+            return "";
+        }
+        
+    }
+
+}
