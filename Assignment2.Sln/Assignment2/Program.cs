3e6fbd346cbe83d4d097da80f4d64de5c4cc537c
TayiebK <150594024+mtk2608@users.noreply.github.com>
2023-12-08 12:54:00 +0200

    Final Commit

--- Assignment2.Sln/Assignment2/Program.cs
+++ Assignment2.Sln/Assignment2/Program.cs
@@ -14,69 +14,6 @@ namespace Assignment2
             Console.WriteLine(Wordels.ConvertNumberToWords(input));
         }
 
-        
-
-
-        /*static string ConvertNumberToWords(int number)
-            {
-                if (number == 0)
-                    return "Zero";
-
-                if (number < 0)
-                    return "Minus " + ConvertNumberToWords(Math.Abs(number));
-
-                string words = "";
-
-                if ((number / 1000) > 0)
-                {
-                    words += ConvertNumberToWords(number / 1000) + " Thousand ";
-                    number %= 1000;
-                }
-
-                if ((number / 100) > 0)
-                {
-                    words += ConvertNumberToWords(number / 100) + " Hundred ";
-                    number %= 100;
-                }
-
-                if (number > 0)
-                {
-                    if (words != "")
-                        words += " ";
-
-                    var unitsMap = new[]
-                    {
-                    "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-                    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
-                    };
-
-                    var tensMap = new[]
-                    {
-                    "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
-                    };
-
-                    if (number < 20)
-                        words += unitsMap[number];
-                    else
-                    {
-                        words += tensMap[number / 10];
-                        if ((number % 10) > 0)
-                            words += "-" + unitsMap[number % 10];
-                    }
-                }
-
-                return words;
-
-                //Console.WriteLine(words);
-            }
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Enter Up To 4 Digits");
-            int input = Convert.ToInt32( Console.ReadLine());
-            Console.WriteLine(ConvertNumberToWords(input));
-            
-        }*/
-
     }
     
 }
--- Assignment2.Sln/Assignment2/Wordels.cs
+++ Assignment2.Sln/Assignment2/Wordels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -14,7 +15,7 @@ namespace Assignment2
         {
 
             string[] SingleDigit =new string[] { " ", "One" , "Two" , "Three" , "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-            string[] DoubleDigit = new string[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+            string[] DoubleDigit = new string[] {"T", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
             string[] Tens = new string[] {" ", " ", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
             if (numbers == 0)
@@ -37,14 +38,71 @@ namespace Assignment2
             else if (numbers > 20 & numbers < 100) 
             {
                 
-                return Tens[numbers/10] + " " + SingleDigit[numbers%10];
+                if (numbers % 10 == 0) 
+                {
+                    return Tens[numbers / 10];
+                }
+                else (numbers>20 & numbers < 99)
+                {
+                    return Tens[numbers / 10] + " " + SingleDigit[numbers % 10];
+                }
                
             }
 
-            /*else if (numbers>100) 
+            else if (numbers>=100 & numbers<999) 
             {
-                return SingleDigit[numbers/100] + " Hundred" + " " + Tens[numbers%100/10] + DoubleDigit[numbers%100%10];
-            }*/
+                if (numbers % 100 == 0)
+                {
+                    return SingleDigit[numbers / 100] + " Hundred ";
+                }
+                else if (numbers % 100 > 0 & numbers % 100 < 10)
+                {
+                    return SingleDigit[numbers / 100] + " Hundred " + " " + SingleDigit[numbers % 10];
+                }
+
+                else if (numbers%100>=10 & numbers%100<20)
+                { 
+                    return SingleDigit[numbers / 100] + " Hundred " + " " + DoubleDigit[numbers%100-10];
+                }
+
+                else if (numbers%100>=20 & numbers % 100 < 99) 
+                {
+                    return SingleDigit[numbers / 100] + " Hundred " + Tens[numbers%100 / 10] + " " + SingleDigit[numbers % 10];
+                }
+
+            }
+
+            else if (numbers>=1000 & numbers<9999) 
+            {
+                if (numbers%1000==0)
+                {
+                    return SingleDigit[numbers/1000] + " Thousand ";
+                }
+                else if (numbers % 1000 > 0 & numbers % 1000 < 10)
+                {
+                    return SingleDigit[numbers / 1000] + " Thousand " + SingleDigit[numbers % 10];
+                }
+
+                else if (numbers%1000>=10 & numbers % 1000 < 20)
+                {
+                    return SingleDigit[numbers / 1000] + " Thousand " +  DoubleDigit[numbers % 1000 - 10];
+                }
+                
+                else if (numbers % 1000%100 >=10 & numbers % 1000%100 <=19)
+                {
+                    return SingleDigit[numbers / 1000] + " Thousand " + SingleDigit[numbers % 1000 / 100] + " Hundred " + DoubleDigit[numbers % 1000 % 100 - 10];
+                }
+                else if (numbers % 1000 >= 20 & numbers % 1000 < 99)
+                {
+                    return SingleDigit[numbers / 1000] + " Thousand " + Tens[numbers % 1000 / 10] + " " + SingleDigit[numbers % 10];
+                }
+                else if (numbers%1000>=100 & numbers%1000 <= 999)
+                {
+                    return SingleDigit[numbers / 1000] + " Thousand " + SingleDigit[numbers % 1000 / 100] + " Hundred " + Tens[numbers % 1000 % 100 / 10] + SingleDigit[numbers % 1000 % 100 % 10] ;
+                }
+                
+
+            }
                 
 
             return "";
